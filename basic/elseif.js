82144ff25a0cf5ca192250e0d376827f05b926c2
Sonu Kumar <krsonu025@gmail.com>
2026-08-11 14:07:49 +0530

    else if and switch statement

--- basic/elseif.js
+++ basic/elseif.js
@@ -19,3 +19,43 @@ if(aa%2==0)
 {
     console.log('number is odd');
 }
+
+// switch statements
+
+
+console.log('switch statements..........................')
+
+let day = 'monday';
+
+switch (day) {
+    case 'monday':
+        console.log('monday');
+        break;
+
+    case 'tuesday':
+        console.log('tuesday');
+        break;
+
+    case 'wednesday':
+        console.log('wednesday');
+        break;
+
+    case 'thursday':
+        console.log('thursday');
+        break;
+
+    case 'friday':
+        console.log('friday');
+        break;
+
+    case 'saturday':
+        console.log('saturday');
+        break;
+
+    case 'sunday':
+        console.log('sunday');
+        break;
+
+    default:
+        console.log('invalid input');
+}
---
+++ basic/loops.js
@@ -0,0 +1,27 @@
+// for loops
+console.log('for in js........................................')
+let a=10;
+for(let i=1; i<=10; i++)
+{
+    console.log(i);
+}
+
+// while loop
+console.log('while loop in javascript.......................')
+let num=1;
+while(num<=10)
+{
+    console.log(num);
+    num++;
+}
+
+// do while-loop
+console.log('do while loop in javascipt')
+let number=1;
+do
+{
+    console.log(number);
+    number++;
+}
+while(number<=10);
+
