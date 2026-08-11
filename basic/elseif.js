1459e72b1afd08adc27cbfb9018a2fda4969ac94
Sonu Kumar <krsonu025@gmail.com>
2026-08-11 13:53:22 +0530

    statement in js

---
+++ basic/elseif.js
@@ -0,0 +1,21 @@
+let a=10;
+let b=5;
+if(a>b)
+{
+    console.log('a is greater than b');
+}else
+{
+    console.log('b is greater than a');
+}
+
+
+// odd evenn number
+
+let aa=10;
+if(aa%2==0)
+{
+    console.log('number is even')
+}else
+{
+    console.log('number is odd');
+}
