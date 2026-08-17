abad9797cd362bf7b6c7b07aca2b09c6726aa7c0
Sonu Kumar <krsonu025@gmail.com>
2026-08-17 15:38:30 +0530

    create model name is user // get and post method created

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURL = "mongodb://127.0.0.1:27017/e-commerse";
+const mongoURL = "mongodb://127.0.0.1:27017/ecommerce";
 
 mongoose.connect(mongoURL);
 
--- server.js
+++ server.js
@@ -1,13 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-// connection to mongodb server.
-const db = require('./db');
+const db = require("./db");// mongodb database server....
+const user = require("./models/user");// user schema
 
-app.get('/', (req, res) => {
-    res.send('Welcome to my E-Commerse Shop............ How can I help you  ?');
+app.use(express.json()); //body parser
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my E-Commerce Shop");
 });
 
+// get method
+app.get("/user",async(req,res)=>{
+    try{
+        const data=await user.find();
+        console.log("data fetched");
+        res.status(200).json(data);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error:"Interval server error"})
+    }
+})
+
+// post method
+app.post("/user",async(req,res)=>{
+    try{
+        const data=req.body;
+        const newUser=new user(data);
+        const response=await newUser.save();
+        console.log("data saved");
+        res.status(201).json(response);
+    }catch(err)
+    {
+        console.log(err);
+        res.status(500).json({error:"Interval server error"})
+    }
+})
+
 app.listen(3000, () => {
-    console.log('Server is listening.........');
+  console.log("Server is listening.........");
 });
